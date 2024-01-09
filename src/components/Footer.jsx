96e62e220f644702197cb41148232772b04aaa3b
Jahz10 <bullymedont.justify@gmail.com>
2024-01-09 14:34:22 +0800

    updated Footer error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="w-full bg-yellow-500/80 py-10 flex flex-col justify-items-center items-center gap-6 font-bold text-white">
       <h1>#JoyFoundHerSoulMate</h1>
-      <p>#GodAnsweredLoonie'sPrayers</p>
+      <p>#GodAnsweredLoonie&apos;sPrayers</p>
       <p>01-15-2023</p>
     </div>
   );
